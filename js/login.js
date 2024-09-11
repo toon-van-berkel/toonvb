de296f94a9654765ca9d9c6907ac22cce019d209
Toon van Berkel <143796002+ToonvanBerkel@users.noreply.github.com>
2024-09-11 16:04:20 +0200

    update

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ function validateLogin() {
     var password = document.getElementById('password').value;
 
     if (username === 'admin-21341' && password === 'AWFOFANWI121422t3wtgww3tg') {
-        window.location.href = './pages/admin.html';
+        window.location.href = './admin.html';
     } else {
         alert('Invalid username or password');
     }
