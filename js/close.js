dc2c4ef3da2b77e9d4ed07d12340696515f02e30
Toon van Berkel <toonvanberkel203@gmail.com>
2024-09-23 14:34:46 +0200

    fixed notification

--- js/close.js
+++ js/close.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let closeButton = document.getElementById('closeButton');
     let notification = document.getElementById('notification');
 
-    closeButton.addEventListener('click', function() {
-        notification.style.display = 'none';
-    });
+    if (closeButton && notification) {
+        closeButton.addEventListener('click', function() {
+            notification.style.display = 'none';
+        });
+    } else {
+        console.error('Elements not found');
+    }
 });
