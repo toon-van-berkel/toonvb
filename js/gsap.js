595e1080b767b73dbbc9b3330001e883e17ac767
Toon van Berkel <toonvanberkel203@gmail.com>
2024-10-24 12:15:04 +0200

    fixed navbar on phone

--- js/gsap.js
+++ js/gsap.js
@@ -1,19 +1,13 @@
-gsap.registerPlugin(ScrollTrigger);
-        gsap.from("#profileImage", {
-            duration: 1.5,
-            scale: 0.5,
-            opacity: 0,
-            ease: "elastic.out(1.5, 1)"
-        });
-    
-        gsap.from("#card1, #card2, #card3, #card4, #card5", {
-            scrollTrigger: {
-                trigger: ".container",
-                start: "top 80%",
-                toggleActions: "restart pause resume pause"
-            },
-            duration: 1,
-            x: -200,
-            opacity: 0,
-            stagger: 0.2
-        });
+gsap.from("#profileImage", {
+    duration: 1.5,
+    scale: 0.5,
+    opacity: 0,
+    ease: "elastic.out(1.5, 1)"
+});
+
+gsap.from("#card1, #card2, #card3, #card4, #card5", {
+    duration: 1,
+    x: -200,
+    opacity: 0,
+    stagger: 0.2
+});
