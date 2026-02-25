3fc82e59b8fee0e9308e16b42c2bd3966c5e8bfd
Toon-van-Berkel <toonvanberkel203@gmail.com>
2026-02-25 19:33:08 +0100

    pathing

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -26,10 +26,16 @@
         </button>
 
         <ul class={"nav__links " + (menuOpen ? "open" : "")}>
-            <li><a href="{base}/Projects" onclick={closeMenu}>Projects</a></li>
-            <li><a href="{base}/Gallery" onclick={closeMenu}>Gallery</a></li>
-            <li><a href="{base}/who-am-i" onclick={closeMenu}>Who am i?</a></li>
-            <li><a href="{base}/contact" onclick={closeMenu}>Contact</a></li>
+            <!-- 
+                <li><a href="{base}/Projects" onclick={closeMenu}>Projects</a></li>
+                <li><a href="{base}/Gallery" onclick={closeMenu}>Gallery</a></li>
+                <li><a href="{base}/who-am-i" onclick={closeMenu}>Who am i?</a></li>
+                <li><a href="{base}/contact" onclick={closeMenu}>Contact</a></li> 
+            -->
+            <li><a href="{base}/" onclick={closeMenu}>Projects</a></li>
+            <li><a href="{base}/" onclick={closeMenu}>Gallery</a></li>
+            <li><a href="{base}/" onclick={closeMenu}>Who am i?</a></li>
+            <li><a href="{base}/" onclick={closeMenu}>Contact</a></li> 
         </ul>
     </div>
 </nav>
