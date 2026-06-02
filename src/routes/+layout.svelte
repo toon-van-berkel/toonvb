d66a218deb7bd22166fa7bc8754bf017f728476c
Toon van Berkel <toonvanberkel203@gmail.com>
2026-06-02 17:35:14 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 	import { loadFunctions } from '$lib/typescript/load';
 
 	let { children } = $props();
 
-	onMount(() => loadFunctions());
+	onMount(() => {
+		loadFunctions();
+	});
 </script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-498Z4JVGVT"></script>
+
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			window.dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-498Z4JVGVT');
+	</script>
+</svelte:head>
 
 {@render children()}
