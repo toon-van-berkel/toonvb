013333be89da0290d9b3592a4f9e0744a16a65e5
Toon van Berkel <toonvanberkel203@gmail.com>
2026-06-02 17:38:42 +0200

    moved linking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,20 +8,4 @@
 		loadFunctions();
 	});
 </script>
-
-<svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-498Z4JVGVT"></script>
-
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			window.dataLayer.push(arguments);
-		}
-
-		gtag('js', new Date());
-		gtag('config', 'G-498Z4JVGVT');
-	</script>
-</svelte:head>
-
 {@render children()}
